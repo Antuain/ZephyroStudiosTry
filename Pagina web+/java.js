91e8eb4337846bec91d6d9dfe28090d2990c9ddc
Antuain <aamaya8975@gmail.com>
2026-05-09 21:01:44 -0300

    Delete Pagina web+ directory

--- Pagina web+/java.js
+++
@@ -1,6 +0,0 @@
-function toggleTexto(boton) {
-    // Busca el overlay que está dentro del mismo slide que el botón
-    const contenedor = boton.parentElement;
-    const overlay = contenedor.querySelector('.video-overlay');
-    overlay.classList.toggle('hidden');
-}
