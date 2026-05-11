f7c63dade5b7cc76ab4825e4bc4d246710161018
Antuain <aamaya8975@gmail.com>
2026-05-11 00:25:27 -0300

    Cambios de gestion en carrusel de movimiento
    se añadio mas control al video y al carrusel y mas interaaccion. y se añadio el trailer del video

--- java.js
+++ java.js
@@ -1,5 +1,16 @@
+const video = document.getElementById('miVideo');
+const overlay = document.getElementById('overlay-text');
+
+video.addEventListener('play', () => {
+    overlay.classList.add('hidden');
+});
+
+
+video.addEventListener('pause', () => {
+    overlay.classList.remove('hidden');
+});
+
 function toggleTexto(boton) {
-    // Busca el overlay que está dentro del mismo slide que el botón
     const contenedor = boton.parentElement;
     const overlay = contenedor.querySelector('.video-overlay');
     overlay.classList.toggle('hidden');
